4a8fa7d3afc0f694663e56a3bb8e36f827722350
miro junker <miro.junker@gmail.com>
2021-05-27 20:15:53 +0200

    Add calculation selector

--- src/components/MyDashboard/RevenueCalculator.tsx
+++ src/components/MyDashboard/RevenueCalculator.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface IRevenueCalculator {
     data: any;
 }
 
-const RevenueCalculator: React.FC<IRevenueCalculator> = ({ data }) => {
-    console.log("RevenuCalculator data", data);
+enum Mode {
+    MAX_REVENUE,
+    MIN_REVENUE,
+}
 
-    return <div>Calculator</div>;
+const DEFAULT_MODE = Mode.MAX_REVENUE;
+
+const RevenueCalculator: React.FC<IRevenueCalculator> = ({ data }) => {
+    console.log("RevenueCalculator data", data);
+
+    const [modeState, setModeState] = useState(DEFAULT_MODE);
+
+    return (
+        <div>
+            <fieldset>
+                <legend>Calculation selector</legend>
+                <select onChange={event => setModeState(Number(event.target.value))}>
+                    <option value={Mode.MAX_REVENUE} selected={modeState === Mode.MAX_REVENUE}>
+                        Maximum Revenue across different products
+                    </option>
+                    <option value={Mode.MIN_REVENUE} selected={modeState === Mode.MIN_REVENUE}>
+                        Minimum Revenue across different products
+                    </option>
+                </select>
+            </fieldset>
+        </div>
+    );
 };
 
 export default RevenueCalculator;
