b8ecc3248c8aac220cab6226cb17ebc4be58e93c
miro junker <miro.junker@gmail.com>
2021-05-27 23:04:31 +0200

    Remove separate filter-options file

--- src/components/MyDashboard/filterOptions.ts
+++
@@ -1,8 +0,0 @@
-import { DateFilterGranularity } from "@gooddata/sdk-backend-spi";
-
-export const availableGranularities: DateFilterGranularity[] = [
-    "GDC.time.date",
-    "GDC.time.month",
-    "GDC.time.quarter",
-    "GDC.time.year",
-];
